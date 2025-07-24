5978440c4488507c76c75d7dd55fa199f870e8c7
NguyenHongMinh1711 <126330168+NguyenHongMinh1711@users.noreply.github.com>
2025-07-24 18:03:29 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       define: {
+        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GOONG_API_KEY': JSON.stringify(env.GOONG_API_KEY),
         'process.env.GOONG_MAPTILES_KEY': JSON.stringify(env.GOONG_MAPTILES_KEY)
